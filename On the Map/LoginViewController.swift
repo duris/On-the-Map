293fe488f02b9aed9513056f326e7b583d2562bb
Ross <ross@duris.io>
2015-08-06 18:27:42 -0400

    Authenticate with Udacity

--- On the Map/LoginViewController.swift
+++ On the Map/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  On the Map
 //
 //  Created by Ross Duris on 8/4/15.
@@ -8,18 +8,154 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var debugTextLabel: UILabel!
+    @IBOutlet weak var headerTextLabel: UILabel!
+    
+    var tapRecognizer: UITapGestureRecognizer? = nil
+    
+    var keyboardAdjusted = false
+    var lastKeyboardOffset : CGFloat = 0.0
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        /* Configure User Interface */
+        self.configureUI()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(animated: Bool) {
+        addKeyboardDismissRecognizer()
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        removeKeyboardDismissRecognizer()
+        unsubscribeToKeyboardNotifications()
     }
 
+    @IBAction func loginButtonTouch() {        
+        UdacityClient.sharedInstance().authenticateWithCredentials(usernameTextField.text, password: passwordTextField.text, hostViewController: self) { (success, sessionID, errorString) in
+            
+            if success {
+                self.completeLogin()
+            } else {
+                self.displayError(errorString)
+            }
+        }
+    }
+    
+    func completeLogin() {
+        dispatch_async(dispatch_get_main_queue(), {
+            self.debugTextLabel.text = ""            
+            let controller = self.storyboard!.instantiateViewControllerWithIdentifier("TabBarController") as! UITabBarController
+            self.presentViewController(controller, animated: true, completion: nil)
+        })
+    }
+    
+    func displayError(errorString: String?) {
+        dispatch_async(dispatch_get_main_queue(), {
+            if let errorString = errorString {
+                self.debugTextLabel.text = errorString
+            }
+        })
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillHide:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if keyboardAdjusted == false {
+            lastKeyboardOffset = getKeyboardHeight(notification) / 2
+            view.superview?.frame.origin.y -= lastKeyboardOffset
+            keyboardAdjusted = true
+        }
+    }
+    
+    func keyboardWillHide(notification: NSNotification) {
+        if keyboardAdjusted == true {
+            view.superview?.frame.origin.y += lastKeyboardOffset
+            keyboardAdjusted = false
+        }
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.CGRectValue().height
+    }
+    
+    func addKeyboardDismissRecognizer() {
+        view.addGestureRecognizer(tapRecognizer!)
+    }
+    
+    func removeKeyboardDismissRecognizer() {
+        view.removeGestureRecognizer(tapRecognizer!)
+    }
+    
+    func handleSingleTap(recognizer: UITapGestureRecognizer) {
+        self.view.endEditing(true)
+    }
 
+    
+    func configureUI() {
+        
+        /* Configure background gradient */
+        self.view.backgroundColor = UIColor.clearColor()
+        let colorTop = UIColor(red: 0.345, green: 0.839, blue: 0.988, alpha: 1.0).CGColor
+        let colorBottom = UIColor(red: 0.023, green: 0.569, blue: 0.910, alpha: 1.0).CGColor
+        var backgroundGradient = CAGradientLayer()
+        backgroundGradient.colors = [colorTop, colorBottom]
+        backgroundGradient.locations = [0.0, 1.0]
+        backgroundGradient.frame = view.frame
+        self.view.layer.insertSublayer(backgroundGradient, atIndex: 0)
+        
+        /* Configure header text label */
+        headerTextLabel.font = UIFont(name: "AvenirNext-Medium", size: 24.0)
+        headerTextLabel.textColor = UIColor.whiteColor()
+        
+        /* Configure email textfield */
+        let emailTextFieldPaddingViewFrame = CGRectMake(0.0, 0.0, 13.0, 0.0);
+        let emailTextFieldPaddingView = UIView(frame: emailTextFieldPaddingViewFrame)
+        usernameTextField.leftView = emailTextFieldPaddingView
+        usernameTextField.leftViewMode = .Always
+        usernameTextField.font = UIFont(name: "AvenirNext-Medium", size: 17.0)
+        usernameTextField.backgroundColor = UIColor(red: 0.702, green: 0.863, blue: 0.929, alpha:1.0)
+        usernameTextField.textColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        usernameTextField.attributedPlaceholder = NSAttributedString(string: usernameTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor.whiteColor()])
+        usernameTextField.tintColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        
+        /* Configure password textfield */
+        let passwordTextFieldPaddingViewFrame = CGRectMake(0.0, 0.0, 13.0, 0.0);
+        let passwordTextFieldPaddingView = UIView(frame: passwordTextFieldPaddingViewFrame)
+        passwordTextField.leftView = passwordTextFieldPaddingView
+        passwordTextField.leftViewMode = .Always
+        passwordTextField.font = UIFont(name: "AvenirNext-Medium", size: 17.0)
+        passwordTextField.backgroundColor = UIColor(red: 0.702, green: 0.863, blue: 0.929, alpha:1.0)
+        passwordTextField.textColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor.whiteColor()])
+        passwordTextField.tintColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        
+        // Configure login button
+        loginButton.titleLabel?.font = UIFont(name: "AvenirNext-Medium", size: 17.0)
+        //loginButton.highlightedBackingColor = UIColor(red: 0.0, green: 0.298, blue: 0.686, alpha:1.0)
+        //loginButton.backingColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        loginButton.backgroundColor = UIColor(red: 0.0, green:0.502, blue:0.839, alpha: 1.0)
+        loginButton.setTitleColor(UIColor.whiteColor(), forState: .Normal)
+        
+        /* Configure tap recognizer */
+        tapRecognizer = UITapGestureRecognizer(target: self, action: "handleSingleTap:")
+        tapRecognizer?.numberOfTapsRequired = 1
+    }
 }
 
--- UdacityClient.swift
+++ UdacityClient.swift
@@ -6,4 +6,134 @@
 //  Copyright (c) 2015 duris.io. All rights reserved.
 //
 
+import UIKit
 import Foundation
+
+class UdacityClient : NSObject {
+    
+    /* Shared session */
+    var session: NSURLSession
+    
+    /* Parsing Error */
+    var parsingError: String!
+    
+    /* Authentication state */
+    var sessionID : String? = nil
+    
+    override init() {
+        session = NSURLSession.sharedSession()
+        super.init()
+    }
+    
+    /* Authenticate with Udacity */
+    func authenticateWithCredentials(username:String, password:String, hostViewController: UIViewController, completionHandler: (success: Bool, sessionID: String?, errorString: String?) -> Void) {
+        
+        /* Configure the request */        
+        /* Note: Without www in the url the request returns registered as false */
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
+        
+        request.HTTPMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPBody = "{\"udacity\": {\"username\": \"\(username)\", \"password\": \"\(password)\"}}".dataUsingEncoding(NSUTF8StringEncoding)
+        
+        /* Make the request */
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil  {
+                //Handle error
+                return
+            } else {
+                
+                /* Subset response data */
+                let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5 ))
+                println(NSString(data: newData, encoding: NSUTF8StringEncoding))
+                
+                /* Parse the data */
+                var parsingError: NSError? = nil
+                let parsedResult = NSJSONSerialization.JSONObjectWithData(newData, options: NSJSONReadingOptions.AllowFragments, error: &parsingError) as! NSDictionary
+                
+                /* Use the data! */
+                if let status = parsedResult["status"] as? Int {
+                    
+                    if status == 400 {
+                        dispatch_async(dispatch_get_main_queue()) {
+                            completionHandler(success: false, sessionID: nil, errorString: "Username or password is empty.")
+                        }
+                    } else if status == 403 {
+                        dispatch_async(dispatch_get_main_queue()) {
+                            completionHandler(success: false, sessionID: nil, errorString: "Account not found or invalid credentials.")
+                        }
+                    }
+                } else {
+                    if let account = parsedResult["account"] as? [String:AnyObject] {
+                        if let account = account["registered"] as? Bool {
+                            if account {
+                                if let session = parsedResult["session"] as? NSDictionary {
+                                    if let sessionID = session["id"] as? String {
+                                        completionHandler(success: true, sessionID: sessionID, errorString: nil)
+                                    } else {
+                                        println("Could not find id in \(session)")
+                                    }
+                                } else {
+                                    println("Could not find session in \(parsedResult)")
+                                }
+                            } else {
+                                println("Account not registered.")
+                            }
+                        } else {
+                            println("Could not find registered in \(account)")
+                        }
+                    } else {
+                        println("Could not find account in \(parsedResult)")
+                    }
+                }
+            }
+        }
+        /* Start the request */
+        task.resume()
+    }
+    
+    func logout(hostViewController: UIViewController) {
+        
+        /* Configure the request */
+        let request = NSMutableURLRequest(URL: NSURL(string: "https://www.udacity.com/api/session")!)
+        request.HTTPMethod = "DELETE"
+        
+        var xsrfCookie: NSHTTPCookie? = nil
+        let sharedCookieStorage = NSHTTPCookieStorage.sharedHTTPCookieStorage()
+        
+        for cookie in sharedCookieStorage.cookies as! [NSHTTPCookie] {
+            if cookie.name == "XSRF-TOKEN" { xsrfCookie = cookie }
+        }
+        
+        if let xsrfCookie = xsrfCookie {
+            request.setValue(xsrfCookie.value!, forHTTPHeaderField: "X-XSRF-TOKEN")
+        }
+    
+        let session = NSURLSession.sharedSession()
+    
+        let task = session.dataTaskWithRequest(request) { data, response, error in
+            if error != nil {
+                // Handle error…
+                return
+            } else {
+                /* Subset response data */
+                let newData = data.subdataWithRange(NSMakeRange(5, data.length - 5))
+                println(NSString(data: newData, encoding: NSUTF8StringEncoding))
+            }
+        }
+        /* Start the request */
+        task.resume()
+    }
+
+    /* Shared Instance */
+    class func sharedInstance() -> UdacityClient {
+        
+        struct Singleton {
+            static var sharedInstance = UdacityClient()
+        }
+        
+        return Singleton.sharedInstance
+    }
+
+}
